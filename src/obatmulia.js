cd3df2417fa6f4aa6ff2f650d2dacb1331d9ac96
colapanmini <colapanmini@gmail.com>
2022-12-28 23:38:19 +0700

    Static Database Added

---
+++ src/obatmulia.js
@@ -0,0 +1,226 @@
+const obatMulia = [
+ {
+    id: 1,
+    name: "ACETYLCYSTEINE" ,
+    image: "https://drive.google.com/file/d/1Kmul-hAlL5VFJYnKnIYljs1d5zVumBDJ/view?usp=share_link"
+ } ,
+{
+    id: 2,
+    name: "ALUGEN 100" ,
+    image: "https://drive.google.com/file/d/1jLUyvFylLbKAqy1VwYnmknpRNJO-2l_N/view?usp=share_link"
+} ,
+{
+    id: 3,
+    name: "ALUGEN 300" ,
+    image: "https://drive.google.com/file/d/1UPzEPbMl3I9WqYyrzSM9JuzAWzKX1Qwx/view?usp=share_link"
+} ,
+{
+    id: 4,
+    name: "AMBROXOL SYRUP" ,
+    image: "https://drive.google.com/file/d/11kiLhS1EsdRrhhihpDFUHrt1rICxg89Z/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMBROXOL TABLET" ,
+    image: "https://drive.google.com/file/d/12boq9SY6TileodObHTGB53oN54JetcUg/view?usp=share_link"
+} ,
+{
+    id: 6,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 7,
+    name: "AMLODIPINE 5 100S" ,
+    image: "https://drive.google.com/file/d/1Qn96BvmFGOMEDusl1cx6Mw5Bju4nSt5Z/view?usp=share_link"
+} , 
+{
+    id: 8,
+    name: "AMLODIPINE 10 30'S" ,
+    image: "https://drive.google.com/file/d/1w986h_T8FaP0Gq6N9la08So4HA7nvRPT/view?usp=share_link"
+} , 
+{
+    id: 9,
+    name: "AMLODIPINE 10 100'S" ,
+    image: "https://drive.google.com/file/d/1W3kBlFJVXlOKwcvjcjHLLbJvS7DsbFpG/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ,
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} , 
+{
+    id: 5,
+    name: "AMLODIPINE 5 30'S" ,
+    image: "https://drive.google.com/file/d/1QVPCDvaNIVbNwDcE_iTZseeVfC9wVhiP/view?usp=share_link"
+} ]
