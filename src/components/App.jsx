4ed5d1b6f43ce1416ffc54f407fb61d8fec4389a
colapanmini <colapanmini@gmail.com>
2023-01-04 23:16:30 +0700

    information changes to featurette

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ function App() {
       <Header />
       <Carousel />
       <div class="container marketing">
+        <h1>Our Products</h1>
+        <p>PT. Mekada Abadi distributes Over-The-Counter (OTC) drugs from PT. Mulia Farma Suci and PT. Mutiara Mukti Farma.</p>
       <Featurettes />
       </div>
     </div>
--- src/components/Featurettes.jsx
+++ src/components/Featurettes.jsx
@@ -7,16 +7,16 @@ function Featurettes() {
           <div className="col-lg-4">
             <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
     
-            <h2 className="fw-normal">Heading</h2>
-            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-            <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+            <h2 className="fw-normal">PT. Mulia Farma Suci</h2>
+            <p>Browse PT. Mulia Farma Suci's products.</p>
+            <p><a className="btn btn-secondary" href="#">Products &raquo;</a></p>
           </div>
           <div className="col-lg-4">
             <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
     
-            <h2 className="fw-normal">Heading</h2>
-            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-            <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+            <h2 className="fw-normal">PT. Mutifa</h2>
+            <p>Browse PT. Mutiara Mukti Farma's products.</p>
+            <p><a className="btn btn-secondary" href="#">Products &raquo;</a></p>
           </div>
           <div className="col-lg-4">
             <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
